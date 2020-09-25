39341d21bc0ba0aa0ab80ec77a603971246e2a81
romain sanson <romainsanson.gm@gmail.com>
2020-09-25 23:59:16 +0200

    new project modal

--- app/javascript/controllers/projects_controller.js
+++ app/javascript/controllers/projects_controller.js
@@ -4,6 +4,10 @@ export default class extends Controller {
 	static targets = ['modal'];
 
 	openModal(event) {
-		console.log(this.modalTarget);
+		this.modalTarget.classList.remove('opacity-0', 'pointer-events-none');
+	}
+
+	closeModal(event) {
+		this.modalTarget.classList.add('opacity-0', 'pointer-events-none');
 	}
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :project, only: %i(index)
+  resources :projects, only: %i(index create)
 
   root to: 'projects#index'
 end
