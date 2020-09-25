88c77ccfd4014a37d49632b628275276c13f6252
romain sanson <romainsanson.gm@gmail.com>
2020-09-26 00:51:35 +0200

    clean

--- app/javascript/controllers/projects_controller.js
+++ app/javascript/controllers/projects_controller.js
@@ -3,11 +3,11 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
 	static targets = ['modal'];
 
-	openModal(event) {
+	openModal() {
 		this.modalTarget.classList.remove('opacity-0', 'pointer-events-none');
 	}
 
-	closeModal(event) {
+	closeModal() {
 		this.modalTarget.classList.add('opacity-0', 'pointer-events-none');
 		this.cleanErrors(this.modalTarget);
 	}
